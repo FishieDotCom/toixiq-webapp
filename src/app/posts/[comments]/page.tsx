cff80fcc476812089496627d060bbe25236b6456
Elgius <114131653+Elgius@users.noreply.github.com>
2024-06-30 16:33:39 +0500

    this is great

--- src/app/posts/[comments]/page.tsx
+++ src/app/posts/[comments]/page.tsx
@@ -1,3 +1,8 @@
+// todo: refactor this because it might be slow in prod
+// note: i am sorry, i speed ran this entire thing so there is a lot to go through
+// then again, this is peak development so...yall have fun
+// tf  counter: 0
+
 "use client";
 
 import {
@@ -13,6 +18,20 @@ import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Logo from "../../../../public/images/toxiq.svg";
 
+interface Post {
+  id: string;
+  type: number;
+  support: number;
+  name: string;
+  userName: string;
+  content: string;
+  commentCount: number;
+  shareCount: number;
+  isDeleted: boolean;
+  postColor: string;
+  wallVersion: number;
+}
+
 interface Data {
   postId: string;
   id: string;
@@ -27,13 +46,35 @@ interface Data {
 export default function Page({ params }: { params: { comments: string } }) {
   const [loading, setLoading] = useState(false);
   const [comments, setComments] = useState<Data[]>([]);
+  const [post, setPost] = useState<Post | null>(null);
 
   let id = params.comments;
   console.log(id);
+
   useEffect(() => {
-    const fetchData = async () => {
+    setLoading(true);
+
+    const fetchDataPosts = async () => {
       try {
-        setLoading(true);
+        const response = await fetch(
+          `https://api.toxiq.xyz/api/Post/GetPost/${id}`
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log(data);
+        setPost(data);
+      } catch (error) {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      }
+    };
+
+    const fetchDataComments = async () => {
+      try {
         const response = await fetch(
           `https://api.toxiq.xyz/api/Comment/GetComments`,
           {
@@ -55,13 +96,16 @@ export default function Page({ params }: { params: { comments: string } }) {
         setComments(result.data);
       } catch (error) {
         console.error(error);
-      } finally {
-        setLoading(false);
       }
     };
 
-    fetchData();
+    // Execute both fetch functions
+    fetchDataPosts();
+    fetchDataComments();
+    setLoading(false);
   }, [id]);
+
+  let bgColor = post?.postColor;
 
   if (loading) {
     return (
@@ -73,7 +117,7 @@ export default function Page({ params }: { params: { comments: string } }) {
 
   return (
     <>
-      <section className="flex items-center justify-center min-h-screen overflow-hidden bg-gray-700">
+      <section className="flex items-center justify-center min-h-screen overflow-hidden bg-black">
         <section className="flex flex-col items-center justify-center">
           <div>
             <div className="flex items-center">
@@ -87,7 +131,31 @@ export default function Page({ params }: { params: { comments: string } }) {
           </div>
 
           <main className="text-center text-white">
-            <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-5 m-5">
+              {post ? (
+                <>
+                  <Card className={`h-[10rem] bg-blue-500`} key={post.id}>
+                    <CardHeader>
+                      <CardTitle className="text-md text-left">
+                        {post.name}
+                      </CardTitle>
+                      <CardDescription className="text-black text-left">
+                        {post.content}
+                      </CardDescription>
+                      <CardContent>
+                        <div className="flex flex-row items-start gap-4 mt-5 px-4 hidden">
+                          <ThumbsUp className="hover:bg-blue-500 hover:text-white transition-colors duration-200 ease-in-out" />
+                          <ThumbsDown className="hover:bg-red-500 hover:text-white transition-colors duration-200 ease-in-out" />
+                        </div>
+                      </CardContent>
+                    </CardHeader>
+                  </Card>
+                </>
+              ) : (
+                <div className="text-white  text-center ">
+                  There seems to be an error
+                </div>
+              )}
               <>
                 {comments.map((comment) => (
                   <Card className="h-[10rem] bg-white" key={comment.id}>
@@ -99,7 +167,7 @@ export default function Page({ params }: { params: { comments: string } }) {
                         {comment.content}
                       </CardDescription>
                       <CardContent>
-                        <div className="flex flex-row items-start gap-4 mt-5 px-4">
+                        <div className="flex flex-row items-start gap-4 mt-5 px-4 hidden">
                           <ThumbsUp className="hover:bg-blue-500 hover:text-white transition-colors duration-200 ease-in-out" />
                           <ThumbsDown className="hover:bg-red-500 hover:text-white transition-colors duration-200 ease-in-out" />
                         </div>
@@ -115,3 +183,5 @@ export default function Page({ params }: { params: { comments: string } }) {
     </>
   );
 }
+
+// test: 91688f4d-cfa2-469d-bf48-d73673242187
